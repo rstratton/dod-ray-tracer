3d4b3ce0533fd9721cbeae8a9df6648ae7e6ae1b
Robert Stratton <r.a.stratton88@gmail.com>
2019-04-25 17:46:31 -0700

    Add reflections

--- DOD Ray Tracer/main.cpp
+++ DOD Ray Tracer/main.cpp
@@ -2,6 +2,7 @@
 #define _USE_MATH_DEFINES
 #include "math.h"
 #include <iostream>
+#include <vector>
 
 #pragma warning(disable:4996)
 
@@ -196,6 +197,7 @@ bool rayIntersectsPlane(Ray r, Plane p, RayHit& h) {
     if (t < 0) return false;
     h.norm = p.norm;
     h.pos = r.pos + t * r.dir;
+    h.dir = r.dir;
     h.hasHit = true;
     return true;
 }
@@ -242,7 +244,45 @@ void computeRayHits(Ray* rays, int numRays, Sphere* spheres, int numSpheres, Pla
     }
 }
 
-void computePointLightDiffuse(RayHit* rayHits, int numRayHits, PointLight light, Vector* diffuseColor) {
+void selectReflectableIntersections(RayHit* rayHits, int numRayHits, std::vector<int>** pReflectableRayHits, std::vector<int>* primaryRayIndices, std::vector<int>** pNewPrimaryRayIndices, int& numReflectableRayHits) {
+    std::vector<int>* reflectableRayHits = new std::vector<int>();
+    *pReflectableRayHits = reflectableRayHits;
+    std::vector<int>* newPrimaryRayIndices = new std::vector<int>();
+    *pNewPrimaryRayIndices = newPrimaryRayIndices;
+
+    reflectableRayHits->reserve(numRayHits);
+    newPrimaryRayIndices->reserve(numRayHits);
+
+    int numReflectable = 0;
+
+    for (int i = 0; i < numRayHits; ++i) {
+        if (rayHits[i].hasHit) {
+            reflectableRayHits->push_back(i);
+            newPrimaryRayIndices->push_back((*primaryRayIndices)[i]);
+            ++numReflectable;
+        }
+    }
+
+    numReflectableRayHits = numReflectable;
+    reflectableRayHits->shrink_to_fit();
+    newPrimaryRayIndices->shrink_to_fit();
+}
+
+void computeReflectionRays(RayHit* rayHits, int numRayHits, Ray** pReflectionRays) {
+    Ray* reflectionRays = new Ray[numRayHits];
+    *pReflectionRays = reflectionRays;
+
+    for (int i = 0; i < numRayHits; ++i) {
+        RayHit hit = rayHits[i];
+
+        Vector v = -hit.dir.normalized();
+        Vector n = hit.norm.normalized();
+        Vector direction = ((n * (2 * v.dot(n))) - v).normalized();
+        reflectionRays[i] = Ray(hit.pos + 0.001f * direction, direction);
+    }
+}
+
+void computePointLightDiffuse(RayHit* rayHits, int numRayHits, PointLight* lights, int numLights, Vector* diffuseColor, Sphere* spheres, int numSpheres, Plane* planes, int numPlanes) {
     for (int rayHitIdx = 0; rayHitIdx < numRayHits; ++rayHitIdx) {
         RayHit hit = rayHits[rayHitIdx];
 
@@ -250,8 +290,38 @@ void computePointLightDiffuse(RayHit* rayHits, int numRayHits, PointLight light,
             continue;
         }
 
-        Vector lightDir = (light.pos - hit.pos).normalized();
-        diffuseColor[rayHitIdx] = diffuseColor[rayHitIdx] + light.color * max(hit.norm.dot(lightDir), 0.f);
+        for (int lightIdx = 0; lightIdx < numLights; ++lightIdx) {
+            PointLight light = lights[lightIdx];
+            Vector lightDiff = light.pos - hit.pos;
+            float lightDistanceSquared = lightDiff.sqmag();
+            Vector lightDir = lightDiff.normalized();
+
+            Ray shadowRay(hit.pos + hit.norm * 0.001f, lightDir);
+            RayHit shadowHit;
+            bool hasHit = false;
+
+            // Spheres
+            for (int sphereIdx = 0; sphereIdx < numSpheres && !hasHit; ++sphereIdx) {
+                if (rayIntersectsSphere(shadowRay, spheres[sphereIdx], shadowHit)) {
+                    if ((shadowHit.pos - hit.pos).sqmag() < lightDistanceSquared) {
+                        hasHit = true;
+                    }
+                }
+            }
+
+            // Planes
+            for (int planeIdx = 0; planeIdx < numPlanes && !hasHit; ++planeIdx) {
+                if (rayIntersectsPlane(shadowRay, planes[planeIdx], shadowHit)) {
+                    if ((shadowHit.pos - hit.pos).sqmag() < lightDistanceSquared) {
+                        hasHit = true;
+                    }
+                }
+            }
+
+            if (!hasHit) {
+                diffuseColor[rayHitIdx] = diffuseColor[rayHitIdx] + light.color * max(hit.norm.dot(lightDir), 0.f);
+            }
+        }
     }
 }
 
@@ -264,6 +334,13 @@ void convertDiffuseToPixels(Vector* diffuse, unsigned char **pPixels, int numPix
         pixels[3 * i]     = (int) min(value.x * 255, 255);
         pixels[3 * i + 1] = (int) min(value.y * 255, 255);
         pixels[3 * i + 2] = (int) min(value.z * 255, 255);
+    }
+}
+
+void integrateReflection(Vector* diffuse, Vector* refDiffuse, int numReflectionRays, std::vector<int>& primaryRayIndices) {
+    for (int i = 0; i < numReflectionRays; ++i) {
+        int diffIdx = primaryRayIndices[i];
+        diffuse[diffIdx] = diffuse[diffIdx] + refDiffuse[i];
     }
 }
 
@@ -284,8 +361,8 @@ int main()
 {
     Camera camera;
     camera.FOV = 50.f;
-    camera.width = 1000;
-    camera.height = 1000;
+    camera.width = 2000;
+    camera.height = 2000;
     camera.pos = { 0.f, 6.f, 20.f };
 
     int numSpheresHorizontal = 5;
@@ -302,17 +379,19 @@ int main()
         }
     }
 
-    int numPlanes = 4;
+    int numPlanes = 6;
     Plane* planes = new Plane[numPlanes];
     planes[0] = { { 0.f, -1.f, 0.f }, { 0.f, 1.f, 0.f } };
     planes[1] = { { 0.f, 0.f, -30.f }, { 0.f, 0.f, 1.f } };
     planes[2] = { { -20.f, 0.f, 0.f }, { 1.f, 0.f, 0.f } };
     planes[3] = { { 20.f, 0.f, 0.f }, { -1.f, 0.f, 0.f } };
+    planes[4] = { { 0.f, 40.f, 0.f }, { 0.f, -1.f, 0.f } };
+    planes[5] = { { 0.f, 0.f, 39.f }, { 0.f, 0.f, -1.f } };
 
     int numLights = 2;
     PointLight* pointLights = new PointLight[numLights];
-    pointLights[0] = { { 100.f, 100.f, 50.f }, { 1.f, 1.f, 0.8f } };
-    pointLights[1] = { { -100.f, 20.f, 20.f }, { 0.8f, 0.8f, 1.f } };
+    pointLights[0] = { { 19.f, 19.f, 1.f }, { 0.07f, 0.07f, 0.05f } };
+    pointLights[1] = { { -19.f, 4.f, 4.f }, { 0.05f, 0.05f, 0.07f } };
 
     // Create primary rays
     int numRays;
@@ -323,16 +402,67 @@ int main()
     RayHit* rayHits;
     computeRayHits(rays, numRays, spheres, numSpheres, planes, numPlanes, &rayHits);
 
-    // Compute diffuse color for ray hits
+    free(rays);
+
+    // Compute direct illumination for primary ray hits
     Vector* diffuse = new Vector[numRays];
     for (int i = 0; i < numRays; ++i) {
         diffuse[i] = { 0.f, 0.f, 0.f };
     }
+    computePointLightDiffuse(rayHits, numRays, pointLights, numLights, diffuse, spheres, numSpheres, planes, numPlanes);
 
-    for (int i = 0; i < numLights; ++i) {
-        computePointLightDiffuse(rayHits, numRays, pointLights[i], diffuse);
+    // Initialize map from rayHit index to primaryRay/pixel index (initialized to identity map)
+    std::vector<int>* primaryRayIndices = new std::vector<int>();
+    primaryRayIndices->resize(numRays);
+    for (int i = 0; i < numRays; ++i) {
+        (*primaryRayIndices)[i] = i;
     }
-    
+
+    int curNumRays = numRays;
+
+    // Compute contribution from reflections
+    for (int i = 0; i < 10; ++i) {
+        // Select rays which intersected with scene objects
+        std::vector<int>* reflectableRayHitIndices;
+        std::vector<int>* newPrimaryRayIndices;
+        int numReflectableRayHits;
+        selectReflectableIntersections(rayHits, curNumRays, &reflectableRayHitIndices, primaryRayIndices, &newPrimaryRayIndices, numReflectableRayHits);
+        free(primaryRayIndices);
+        primaryRayIndices = newPrimaryRayIndices;
+
+        RayHit* reflectableRayHits = new RayHit[numReflectableRayHits];
+        for (int j = 0; j < numReflectableRayHits; ++j) {
+            reflectableRayHits[j] = rayHits[(*reflectableRayHitIndices)[j]];
+        }
+
+        free(rayHits);
+
+        Ray* reflectionRays;
+        computeReflectionRays(reflectableRayHits, numReflectableRayHits, &reflectionRays);
+
+        free(reflectableRayHits);
+
+        // Compute reflection ray hits
+        RayHit* reflectionRayHits;
+        computeRayHits(reflectionRays, numReflectableRayHits, spheres, numSpheres, planes, numPlanes, &reflectionRayHits);
+
+        free(reflectionRays);
+
+        // Compute diffuse color for reflection ray hits
+        Vector* refDiffuse = new Vector[numReflectableRayHits];
+        for (int i = 0; i < numReflectableRayHits; ++i) {
+            refDiffuse[i] = { 0.f, 0.f, 0.f };
+        }
+        computePointLightDiffuse(reflectionRayHits, numReflectableRayHits, pointLights, numLights, refDiffuse, spheres, numSpheres, planes, numPlanes);
+
+        rayHits = reflectionRayHits;
+        curNumRays = numReflectableRayHits;
+
+        // Integrate diffuse from reflection rays with primary rays
+        integrateReflection(diffuse, refDiffuse, numReflectableRayHits, *primaryRayIndices);
+        free(refDiffuse);
+    }
+
     unsigned char* pixels;
     convertDiffuseToPixels(diffuse, &pixels, numRays);
     writePPM(pixels, camera.width, camera.height, "..\\renders\\image.ppm");
